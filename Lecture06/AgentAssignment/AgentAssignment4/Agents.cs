59e7abece5f2158368c3e0d70b7e1978a31274f4
Kasper Juul Hermansen <hermansendesigns@gmail.com>
2018-02-17 17:39:37 +0100

    Finished 4

---
+++ Lecture06/AgentAssignment/AgentAssignment4/Agents.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace AgentAssignment4
+{
+    //
+    public class Agents : ObservableCollection<Agent>, INotifyPropertyChanged
+    {
+        public Agents()
+        {
+            Add(new Agent("001", "Nina", "Assassination", "UpperVolta"));
+            Add(new Agent("007", "James Bond", "Martinis", "North Korea"));
+        }
+
+        #region Commands
+
+        ICommand _addCommand;
+        public ICommand AddCommand
+        {
+            get { return _addCommand ?? (_addCommand = new RelayCommand(AddAgent)); }
+        }
+
+        private void AddAgent()
+        {
+            Add(new Agent());
+            CurrentIndex = Count - 1;
+        }
+
+
+        ICommand _deleteCommand;
+        public ICommand DeleteCommand
+        {
+            get { return _deleteCommand ?? (_deleteCommand = new RelayCommand(DeleteAgent, DeleteAgent_CanExecute)); }
+        }
+
+        private void DeleteAgent()
+        {
+            RemoveAt(CurrentIndex);
+        }
+
+        private bool DeleteAgent_CanExecute()
+        {
+            if (Count > 0 && CurrentIndex >= 0)
+                return true;
+            else
+                return false;
+        }
+
+        ICommand _nextCommand;
+        public ICommand NextCommand
+        {
+            get
+            {
+                return _nextCommand ?? (_nextCommand = new RelayCommand(
+                    () => ++CurrentIndex,
+                    () => CurrentIndex < (Count - 1)));
+            }
+        }
+
+        ICommand _PreviusCommand;
+        public ICommand PreviusCommand
+        {
+            get { return _PreviusCommand ?? (_PreviusCommand = new RelayCommand(PreviusCommandExecute, PreviusCommandCanExecute)); }
+        }
+
+        private void PreviusCommandExecute()
+        {
+            if (CurrentIndex > 0)
+                --CurrentIndex;
+        }
+
+        private bool PreviusCommandCanExecute()
+        {
+            if (CurrentIndex > 0)
+                return true;
+            else
+                return false;
+        }
+
+        #endregion // Commands
+
+        #region Properties
+
+        int currentIndex = -1;
+
+        public int CurrentIndex
+        {
+            get { return currentIndex; }
+            set
+            {
+                if (currentIndex != value)
+                {
+                    currentIndex = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        #endregion
+
+        #region INotifyPropertyChanged implementation
+
+        public new event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+    }
+}
