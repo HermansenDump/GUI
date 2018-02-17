ab0a848a13a3469f93e7f23229d508004007a035
Kasper Juul Hermansen <hermansendesigns@gmail.com>
2018-02-17 18:38:02 +0100

    Finished 6 All Done! Yay!

---
+++ Lecture06/AgentAssignment/AgentAssignment6/Clock.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using AgentAssignment6.Annotations;
+
+namespace AgentAssignment6
+{
+    public class Clock : INotifyPropertyChanged
+    {
+        private string date;
+        private string time;
+
+        public Clock()
+        {
+            Update();
+        }
+
+        public void Update()
+        {
+            Date = DateTime.Now.ToLongDateString();
+            Time = DateTime.Now.ToLongTimeString();
+        }
+
+        public string Date
+        {
+            get
+            {
+                return date;
+
+            }
+            private set {
+                if (date != value)
+                {
+                    date = value;
+                    Notify("Date");
+                }
+            }
+        }
+
+        public string Time
+        {
+            get
+            {
+                return time;
+
+            }
+            private set
+            {
+                if (time != value)
+                {
+                    time = value;
+                    Notify("Time");
+                }
+            }
+        }
+
+        private void Notify(string propName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ Lecture06/AgentAssignment/AgentAssignment6/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace AgentAssignment6
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer timer = new DispatcherTimer();
+        Clock clock = new Clock();
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            SpClock.DataContext = clock;
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += new EventHandler(Timer_Tick);
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            clock.Update();
+        }
+
+        private void CloseCommand_OnExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
